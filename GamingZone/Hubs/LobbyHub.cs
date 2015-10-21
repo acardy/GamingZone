c3b1fa5858756434f18419d826d73a80aac9beb9
Andrew Cardy <andy.cardy@gmail.com>
2015-10-21 13:34:47 +0100

    Starting to persist lobby table state.

--- GamingZone/Hubs/LobbyHub.cs
+++ GamingZone/Hubs/LobbyHub.cs
@@ -10,6 +10,7 @@ namespace GamingZone.Hubs
     public class LobbyHub : Hub
     {
         private static readonly List<UserConnection> m_connections = new List<UserConnection>();
+        private static readonly List<LobbyTables> m_lobbyTables = new List<LobbyTables>();
 
         public async Task Join()
         {
@@ -38,6 +39,15 @@ namespace GamingZone.Hubs
             var userConnection = m_connections.FirstOrDefault(g => g.ConnectionId == Context.ConnectionId);
             if (userConnection == null)
                 return;
+
+            var lobbyTable = m_lobbyTables.FirstOrDefault(lt => lt.LobbyId == userConnection.GroupId);
+            if (lobbyTable == null)
+            {
+                lobbyTable = new LobbyTables(userConnection.GroupId, 250, 2);
+                m_lobbyTables.Add(lobbyTable);
+            }
+
+            var seat = lobbyTable.Tables[tableIndex].Seats[seatIndex];
 
             // Need to add some state somewhere.
             Clients.Group(userConnection.GroupId).UserSat(tableIndex, seatIndex, userConnection.Name);
@@ -70,5 +80,42 @@ namespace GamingZone.Hubs
             public string ConnectionId { get; private set; }
             public string GroupId { get; private set; }
         }
+
+        private class LobbyTables
+        {
+
+            public LobbyTables(string lobbyId, int numberOfTables, int seatsPerTable)
+            {
+                LobbyId = lobbyId;
+
+                Tables = new List<Table>();
+
+                for (int tableIndex = 0; tableIndex < numberOfTables; tableIndex++)
+                    Tables.Add(new Table(seatsPerTable));
+            }
+
+            public string LobbyId { get; internal set; }
+            public List<Table> Tables { get; private set; }
+        }
+
+        private class Table
+        {
+            public Table(int numberOfSeats)
+            {
+                Seats = new List<Seat>();
+
+                for (int tableIndex = 0; tableIndex < numberOfSeats; tableIndex++)
+                    Seats.Add(new Seat());
+            }
+
+            public List<Seat> Seats { get; private set; }
+        }
+
+        private class Seat
+        {
+            public bool ThumbsUp { get; set; }
+            public UserConnection User { get; set; }
+        }
     }
+
 }
